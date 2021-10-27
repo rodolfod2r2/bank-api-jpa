dbdf3bc44a36d3a7f19e21d8f5d749c35d3d7e74
Rodolfo Gonçalves de Luna Freire <rodolfod2r2@gmail.com>
2021-10-26 21:36:19 -0300

    Initial Commit

--- src/main/java/org/framework/git/bank/BankApplication.java
+++ src/main/java/org/framework/git/bank/BankApplication.java
@@ -2,10 +2,8 @@ package org.framework.git.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
 public class BankApplication {
 
     public static void main(String[] args) {
